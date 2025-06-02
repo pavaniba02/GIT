a0a1ec84a187ea36107ca5d6852f2c8495d120c3
Pavani B A <pavani.a@s.amity.edu>
2025-06-02 14:56:31 +0530

    changed world to earth

--- Hello.cpp
+++ Hello.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout<<"Hello World"<<endl;
+	cout<<"Hello earth"<<endl;
 	cout<<"Hello!!"<<endl;
 	cout<<"This is SCM"<<endl;
 	cout<<"Welcome to GIT!!"<<endl;
